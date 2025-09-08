314762120fdf8d5d1a0fbcccfefc8613b6cf1c14
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2025-09-08 18:52:19 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, me = 0, p = 0;
+    scanf("%d", &n);
+    int x[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d ", &x[i]);
+        
+    }
+    me = x[0];
+    for(int j = 0; j < n; j++){
+        if(x[j] <= me){
+            me = x[j];
+            p = j;
+        }
+    }
+    printf("Menor valor: %d\n", me);
+    printf("Posicao: %d\n", p);
+    
+    return 0;
+}
