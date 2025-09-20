9a55147d3b5cf66feba89693077e81a850948d53
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2025-09-19 21:52:48 -0300

    Create 1180c.c

---
+++ 1180c.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, me = 0, p = 0, *ponteiro;
+    scanf("%d", &n);
+
+    int *x;
+    x = (int *) malloc(n * sizeof(int));
+
+    if (x == NULL) {
+        return 1;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &x[i]);
+    }
+
+    me = *x; 
+
+    for (ponteiro = x; ponteiro < x + n; ponteiro++) {
+        if (*ponteiro <= me) {
+            me = *ponteiro;
+            p = ponteiro - x;
+        }
+    }
+
+    printf("Menor valor: %d\n", me);
+    printf("Posicao: %d\n", p);
+
+    free(x);
+
+    return 0;
+}
