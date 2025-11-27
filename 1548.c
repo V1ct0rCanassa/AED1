84549620ac91d4c1f8f363052560e806c0b6cf76
Vpn210 <103062205+V1ct0rCanassa@users.noreply.github.com>
2025-11-26 23:42:59 -0300

    Implement sorting and counting of original array

---
+++ 1548.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int comparar(const void *a, const void *b) {
+    return (*(int*)b - *(int*)a);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    while (N--) {
+        int M;
+        scanf("%d", &M);
+
+        int original[1005];
+        int ordenado[1005];
+
+        for (int i = 0; i < M; i++) {
+            scanf("%d", &original[i]);
+            ordenado[i] = original[i];
+        }
+
+        qsort(ordenado, M, sizeof(int), comparar);
+
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            if (original[i] == ordenado[i]) {
+                count++;
+            }
+        }
+
+        printf("%d\n", count);
+    }
+
+    return 0;
+}
